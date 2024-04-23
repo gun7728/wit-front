274e18a8bba504d690031bb0636def05df062122
gun7728 <gun7728@gmail.com>
2024-04-23 05:07:39 +0000

    Update main.yml

--- src/app/containers/App.tsx
+++ src/app/containers/App.tsx
@@ -1,16 +1,34 @@
-import React from "react";
-import '../../App.css'
-import '@picocss/pico'
-import {useThemeStore} from "../../store/theme";
-export const App = () => {
-  const { setTheme } = useThemeStore();
-  return (
-      <div style={{width:'100%', height:'100vh'}}>
-        <button onClick={()=>setTheme('light')}>
-        </button>
-        <button onClick={()=>setTheme('dark')}>
-        </button>
-      </div>
-  )
-};
+import React, { useEffect } from 'react';
+import '../../App.scss';
+import styled from 'styled-components';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { WitMap } from 'app/containers/pages/map/WitMap';
+import { WitMain } from 'app/containers/pages/main/WitMain';
 
+const AppPage = styled.div`
+  width: 100%;
+  height: calc(var(--vh, 1vh) * 100);
+`;
+
+export const App = () => {
+  function setScreenSize() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  useEffect(() => {
+    setScreenSize();
+  });
+
+  return (
+    <AppPage>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navigate to='/main' />} />
+          <Route path='/main' element={<WitMain />} />
+          <Route path='/map' element={<WitMap />} />
+        </Routes>
+      </BrowserRouter>
+    </AppPage>
+  );
+};
